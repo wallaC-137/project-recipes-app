d2e340af08e127f632d3d5ba65e8e5d195dbdff4
rrodrigodlima <rdrigo019@gmail.com>
2023-03-08 18:34:26 -0300

    refatored 44 and created a recipe card

--- src/Pages/DoneRecipes.jsx
+++ src/Pages/DoneRecipes.jsx
@@ -1,37 +1,8 @@
-import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
-import drinkIcon from '../images/drinkIcon.svg';
+import RecipesCard from '../components/RecipesCard';
 
 function DoneRecipes() {
-  const [data] = useState([{
-    id: '0',
-    type: 'drink',
-    nationality: 'brazil',
-    category: 'hot',
-    alcoholicOrNot: 'alcoholic',
-    name: 'Caipitinha',
-    image: 'imagem',
-    doneDate: 'data',
-    tags: 'tag',
-  },
-  {
-    id: '1',
-    type: 'drink',
-    nationality: 'mexico',
-    category: 'vazio',
-    alcoholicOrNot: 'alcoholic',
-    name: 'tequila',
-    image: 'imagem2',
-    doneDate: 'quando2',
-    tags: 'tag2',
-  }]);
-
-  useEffect(() => {
-    localStorage.setItem('doneRecipes', JSON.stringify(data));
-  }, [data]);
-
   const recipes = JSON.parse(localStorage.getItem('doneRecipes'));
-
   return (
     <div>
       <Header />
@@ -51,23 +22,9 @@ function DoneRecipes() {
         >
           Drinks
         </button>
-
-        <img src={ drinkIcon } alt="imge" data-testid={ `0-horizontal-image` } />
-
-        <p data-testid={ `0-horizontal-top-text` }>Category text</p>
-
-        <p data-testid={ `0-horizontal-name` }>recipe name</p>
-
-        <p data-testid={ `0-horizontal-done-date` }>
-          data q a receita foi feita
-        </p>
-
-        <button type="button" data-testid={ `0-horizontal-share-btn` }>
-          Share
-        </button>
-        <p>Tags</p>
-        <p data-testid={ `0-Pasta-horizontal-tag` }>Pasta</p>
-        <p data-testid={ `0-Curry-horizontal-tag` }>Curry</p>
+        <br />
+        <br />
+        <RecipesCard recipes={ recipes } />
       </div>
     </div>
   );
---
+++ src/components/RecipesCard.jsx
@@ -0,0 +1,50 @@
+import { number, arrayOf, string, shape } from 'prop-types';
+import shareIcon from '../images/shareIcon.svg';
+
+function RecipesCard({ recipes }) {
+  return (
+    <div>
+      {recipes.map((recipe, index) => (
+        <div key={ index }>
+          <img
+            src={ recipe.image }
+            alt={ recipe.name }
+            data-testid={ `${index}-horizontal-image` }
+          />
+          <h1 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h1>
+          <p data-testid={ `${index}-horizontal-top-text` }>{recipe.categoy}</p>
+          <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+          {recipe.tags.map((tag) => (
+            <p data-testid={ `${index}-${tag}-horizontal-tag` } key={ index }>
+              {tag}
+            </p>
+          ))}
+          <button
+            src={ shareIcon }
+            type="button"
+            data-testid={ `${index}-horizontal-share-btn` }
+          >
+            <img src={ shareIcon } alt="share-icon" />
+          </button>
+
+        </div>
+      ))}
+    </div>
+  );
+}
+
+RecipesCard.propTypes = {
+  recipes: arrayOf(
+    shape({
+      id: number,
+      name: string,
+      image: string,
+      category: string,
+      doneDate: string,
+      type: string,
+      tags: arrayOf(string),
+    }),
+  ),
+}.isRequired;
+
+export default RecipesCard;
